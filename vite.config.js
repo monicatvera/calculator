16a2c7010c90e0c1fe544204e548366e9fd7efb1
Mónica Ilenia Tardón Vera <monitvera@gmail.com>
2022-11-01 23:30:31 +0000

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/calculator/',
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
